dba66f8438ba717eb947aaa2b2e4ee8bbb8ba65c
Fabian Pena <camilo.fabis@gmail.com>
2020-09-02 15:42:19 -0400

    first iteration

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+const canvas = document.querySelector('canvas');
+canvas.width = 500;
+canvas.height = 700;
+const ctx = canvas.getContext('2d');
+const roadImg = new Image();
+roadImg.src = '/images/road.png'
 
-  function startGame() {}
-};
+roadImg.onload = () => {
+  ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height)
+}
