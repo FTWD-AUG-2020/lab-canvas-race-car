ce08bc7dea41ac3b4e5b2fa4f6ade9f3f46f8856
Ashtyn Ce <ashtyncza@gmaile.com>
2020-09-02 16:28:45 -0400

    ashtynroxevenmore

--- js/index.js
+++ js/index.js
@@ -7,11 +7,41 @@ carImg.src = "/images/car.png";
 const roadImg = new Image();
 roadImg.src = "/images/road.png";
 
+
+class Car {
+  constructor(img, x, y, w, h) {
+    this.img = img
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+  }
+  drawCar() {
+    ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
+  }
+}
+
+let miniCooper = new Car(carImg, 250, 600, 50, 100)
+
+
+window.onkeydown = function(event) {
+  console.log(event.key)
+  switch(event.key){
+    case"ArrowLeft": 
+    miniCooper.x -= 7
+    break;
+    case"ArrowRight":
+    miniCooper.x += 7
+    break;
+
+  }
+}
+
 function animate() {
   window.requestAnimationFrame(animate);
   console.log("gameloop");
   ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(carImg, 250, 600, 50, 100);
+  miniCooper.drawCar() 
 }
 
 animate();
