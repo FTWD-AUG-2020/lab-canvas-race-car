24427e46953c1747b586abc6f0046a0305b0b6a8
second login <secondary@Matthews-MacBook-Pro-2.local>
2020-09-02 16:01:17 -0400

    racecarrrr

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 canvas.width = 500;
 canvas.height = 700;
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
+const carImg = new Image();
+carImg.src = "/images/car.png";
 const roadImg = new Image();
-roadImg.src = '/images/road.png'
+roadImg.src = "/images/road.png";
 
-roadImg.onload = () => {
-  ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height)
+function animate() {
+  window.requestAnimationFrame(animate);
+  console.log("gameloop");
+  ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(carImg, 250, 600, 50, 100);
 }
+
+animate();
